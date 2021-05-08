63bfc049e1915582bc2b91d7d6aad5b0f948cc35
Onur Caglayan <62426634+onurmx@users.noreply.github.com>
2021-05-08 13:48:04 +0300

    Initial commit

---
+++ WPFLAB/ArrivalTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPFLAB
+{
+    class ArrivalTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return ((Flight)value).DepartureTime.Add(((Flight)value).Duration);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPFLAB/WeightConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPFLAB
+{
+    class WeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (((int)value) * 1000).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
